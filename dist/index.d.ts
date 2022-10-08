6a0d3047fa80408740ffc91caaa4acbc16a2c871
Mike Hockerman <mike@hockerman.com>
2022-10-08 19:32:05 -0400

    Emit types.

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+import * as io from "io-ts";
+import * as fp from "fp-ts";
+export { io, fp };
+export * from "./iofetch";
---
+++ dist/iofetch.d.ts
@@ -0,0 +1,28 @@
+/// <reference types="@cloudflare/workers-types" />
+import * as io from "io-ts";
+export interface IOFetchJSONSuccess<B> {
+    body: B;
+    response: Response;
+}
+/**
+ * iofetch_json treates the generic as the schema that the json body of the response from the fetch
+ * should take. Internally, it runs the fetch, parses the json body of the result, schematizes it
+ * per the schema provided, then returns the body fully typed.
+ *
+ * This function will throw an error in any failure condition; including if the fetch fails (exactly
+ * how `fetch` itself behaves); if the call to `response.json()` fails; or if the response body does
+ * not match the schema.
+ *
+ * In the final case, when the response body does not match the schema; the error message will be a
+ * reasonably pretty error message that describes how the response body and schema do not match.
+ *
+ * ```ts
+ * import { io, iofetch_json } from "@mhoc/workers-io"
+ * const ResponseJSON = io.type({
+ *   userId: io.string,
+ * })
+ * const { request, body } = await iofetch_json(ResponseJSON, "https://api.example.com/getUser");
+ * console.log(body.userId) // compiles, and guaranteed to be a string
+ * ```
+ */
+export declare const iofetch_json: <B extends io.TypeC<any>>(spec: B, request: Request, requestInitr?: RequestInit | Request) => Promise<IOFetchJSONSuccess<B>>;
